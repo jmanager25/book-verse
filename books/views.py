aac1f87220108df5f75c09595cb79fc6c4d1b01b
jmanager25 <jmanager25@hotmail.com>
2023-10-01 17:00:12 +0000

    Update book view

--- books/views.py
+++ books/views.py
@@ -7,11 +7,14 @@ from bookverse_api.permissions import IsOwnerOrReadOnly
 
 class BookListView(generics.ListCreateAPIView):
     """
-    List all books
+    List all books or create a new book
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
