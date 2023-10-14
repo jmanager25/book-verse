97994e172b46868d520cb88aaaae849846f834f7
jmanager25 <jmanager25@hotmail.com>
2023-10-14 06:14:12 +0000

    Add modal to comfirm review deletion

--- frontend/src/pages/reviews/Review.js
+++ frontend/src/pages/reviews/Review.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../../styles/Review.module.css';
 import { useCurrentUser } from '../../context/currentUserContext';
 import { Card, Media } from 'react-bootstrap';
@@ -8,6 +8,7 @@ import StarRating from '../../components/StarRating';
 import { MoreDropdown } from '../../components/MoreDropdown';
 import { axiosRes } from '../../api/axiosDefaults';
 import useAlert from '../../hooks/useAlert';
+import DeleteConfirmationModal from '../../components/DeleteConfirmationModal';
 
 
 const Review = (props) => {
@@ -26,21 +27,31 @@ const Review = (props) => {
   const is_owner = currentUser?.username === owner;
   const history = useHistory();
   const { setAlert } = useAlert();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
+  const [selectedReview, setSelectedReview] = useState(null);
 
   const handleEdit = () => {
     history.push(`/books/${book}/reviews/${id}/edit`)
   }
 
+  const openDeleteModal = (id) => {
+    setSelectedReview(id);
+    setShowDeleteModal(true); 
+  };
+
   const handleDelete = async () => {
-    try {
-      // temporary solution to delete review, does not work on deployed project bvecause the url is different
-      await axiosRes.delete(`https://8080-jmanager25-bookverse-1zsn2srl0z9.ws-eu105.gitpod.io/api/reviews/${id}`);
-      // Find a better solution because reloading the entire page gives poor user expirience
-      window.location.reload();
-      setAlert('Review deleted succesfuly', 'success')
-    } catch (err) {
-      console.log(err)
-      setAlert('Failed to delete review', 'error')
+    if (selectedReview) {
+      try {
+        // temporary solution to delete review, does not work on deployed project bvecause the url is different
+        await axiosRes.delete(`https://8080-jmanager25-bookverse-1zsn2srl0z9.ws-eu105.gitpod.io/api/reviews/${id}`);
+        // Find a better solution because reloading the entire page gives poor user expirience
+        window.location.reload()
+        setAlert('Review deleted succesfuly', 'success')
+      } catch (err) {
+        console.log(err)
+        setAlert('Failed to delete review', 'error')
+      }
+      setShowDeleteModal(false);
     }
   };
 
@@ -57,7 +68,7 @@ const Review = (props) => {
                 </div>
                 <div>
                   <span>{updated_at}</span>
-                  {is_owner && <MoreDropdown handleEdit={handleEdit} handleDelete={handleDelete} />}
+                  {is_owner && <MoreDropdown handleEdit={handleEdit} handleDelete={openDeleteModal} />}
                 </div>
             </Media>
             <Card.Text className={styles.Text}>{review_text}</Card.Text>
@@ -66,6 +77,11 @@ const Review = (props) => {
             <span><i className='far fa-heart' /> Like</span>
             <span><i className='far fa-comments' /> Comment</span>
         </div>
+        <DeleteConfirmationModal
+            show={showDeleteModal}
+            handleClose={() => setShowDeleteModal(false)}
+            handleConfirm={handleDelete}
+        />
     </Card>
   )
 }
