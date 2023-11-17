9bc4fbba55459414b472e4d1a26d73b56a674bfe
jmanager25 <jmanager25@hotmail.com>
2023-11-17 13:45:39 +0000

    Update time format

--- bookverse_api/settings.py
+++ bookverse_api/settings.py
@@ -33,8 +33,13 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication'
         if 'DEV' in os.environ
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
-    )]
+    )],
+    'DATETIME_FORMAT': '%d %b %Y',
 }
+if 'DEV' not in os.environ:
+    REST_FRAMEWORK['DEFAULT_RENDERER_CLASSES'] = [
+        'rest_framework.renderers.JSONRenderer',
+    ]
 
 REST_USE_JWT = True
 JWT_AUTH_SECURE = True
--- bookverse_api/urls.py
+++ bookverse_api/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
+from .views import root_route
 
 urlpatterns = [
+    path('', root_route),
     path('', TemplateView.as_view(template_name='index.html')),
     path('admin/', admin.site.urls),
     path('api/api-auth/', include('rest_framework.urls')),
