f677fc68de35e779a9ab4fc8b4b95ac5d927e5f6
jmanager25 <jmanager25@hotmail.com>
2023-11-19 20:32:01 +0000

    Fix validation errors on settings.py

--- TESTING.md
+++ TESTING.md
@@ -18,10 +18,11 @@
 | Models | [CI PEP8](https://pep8ci.herokuapp.com/https://raw.githubusercontent.com/jmanager25/book-verse/main/books/models.py) | Pass |
 | apps | [CI PEP8](https://pep8ci.herokuapp.com/https://raw.githubusercontent.com/jmanager25/book-verse/main/books/apps.py) | Pass |
 | **Bookverse_api** |
-| Views | [CI PEP8]() | Pass |
-| Views | [CI PEP8]() | Pass |
-| Views | [CI PEP8]() | Pass |
-| Views | [CI PEP8]() | Pass |
+| Views | [CI PEP8](https://pep8ci.herokuapp.com/https://raw.githubusercontent.com/jmanager25/book-verse/main/bookverse_api/views.py) | Pass |
+| Urls | [CI PEP8](https://pep8ci.herokuapp.com/https://raw.githubusercontent.com/jmanager25/book-verse/main/bookverse_api/urls.py) | Pass |
+| Serializers | [CI PEP8]() | Pass |
+| Permissions | [CI PEP8]() | Pass |
+| **Comments** |
 | Views | [CI PEP8]() | Pass |
 | Views | [CI PEP8]() | Pass |
 | Views | [CI PEP8]() | Pass |
--- bookverse_api/settings.py
+++ bookverse_api/settings.py
@@ -47,7 +47,7 @@ JWT_AUTH_COOKIE = 'my-app-auth'
 JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'bookverse_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'bookverse_api.serializers.CurrentUserSerializer'  # noqa
 }
 
 # Quick-start development settings - unsuitable for production
@@ -153,16 +153,16 @@ else:
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',  # noqa
     },
 ]
 
